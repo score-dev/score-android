f537704a319901f56ef4d1cdf614fe22f952f4de
PIYUJIN <yjyj020201@gmail.com>
2024-08-09 22:34:22 +0900

    [Feat] #1  splash 화면 구성

---
+++ app/src/main/java/com/project/score/onBoarding/SplashFragment.kt
@@ -0,0 +1,29 @@
+package com.project.score.onBoarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.project.score.R
+import com.project.score.databinding.FragmentOnboarding2Binding
+import com.project.score.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    lateinit var binding: FragmentSplashBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentSplashBinding.inflate(layoutInflater)
+
+        binding.run {
+            
+        }
+
+        return binding.root
+    }
+}
