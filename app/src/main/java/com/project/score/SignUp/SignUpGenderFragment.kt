49497771986d82ced7350311ad269fd5e5abbafd
PIYUJIN <yjyj020201@gmail.com>
2025-03-22 02:05:47 +0900

    [Feat] #18  회원가입 - 성별 선택 화면 기능 구현

--- app/src/main/java/com/project/score/SignUp/SignUpGenderFragment.kt
+++ app/src/main/java/com/project/score/SignUp/SignUpGenderFragment.kt
@@ -5,56 +5,125 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.project.score.OnBoarding.OnboardingActivity
 import com.project.score.R
+import com.project.score.Utils.MyApplication
+import com.project.score.databinding.FragmentSignUpGenderBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SignUpGenderFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SignUpGenderFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    lateinit var binding: FragmentSignUpGenderBinding
+    lateinit var onboardingActivity: OnboardingActivity
+
+    var gender: String? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up_gender, container, false)
+        binding = FragmentSignUpGenderBinding.inflate(layoutInflater)
+        onboardingActivity = activity as OnboardingActivity
+
+        initView()
+
+        binding.run {
+            buttonGenderFemale.setOnClickListener {
+                buttonNext.visibility = View.VISIBLE
+                when(gender) {
+                    "MALE" -> {
+                        buttonGenderMale.run {
+                            backgroundTintList = resources.getColorStateList(R.color.grey2)
+                            setTextColor(resources.getColor(R.color.text_color3))
+                        }
+                    }
+                    "OTHER" -> {
+                        buttonGenderOthers.run {
+                            backgroundTintList = resources.getColorStateList(R.color.grey2)
+                            setTextColor(resources.getColor(R.color.text_color3))
+                        }
+                    }
+                }
+                buttonGenderFemale.run {
+                    backgroundTintList = resources.getColorStateList(R.color.main)
+                    setTextColor(resources.getColor(R.color.white))
+                }
+                gender = "FEMALE"
+            }
+
+            buttonGenderMale.setOnClickListener {
+                buttonNext.visibility = View.VISIBLE
+                when(gender) {
+                    "FEMALE" -> {
+                        buttonGenderFemale.run {
+                            backgroundTintList = resources.getColorStateList(R.color.grey2)
+                            setTextColor(resources.getColor(R.color.text_color3))
+                        }
+                    }
+                    "OTHER" -> {
+                        buttonGenderOthers.run {
+                            backgroundTintList = resources.getColorStateList(R.color.grey2)
+                            setTextColor(resources.getColor(R.color.text_color3))
+                        }
+                    }
+                }
+                buttonGenderMale.run {
+                    backgroundTintList = resources.getColorStateList(R.color.main)
+                    setTextColor(resources.getColor(R.color.white))
+                }
+                gender = "MALE"
+            }
+
+            buttonGenderOthers.setOnClickListener {
+                buttonNext.visibility = View.VISIBLE
+                when(gender) {
+                    "FEMALE" -> {
+                        buttonGenderFemale.run {
+                            backgroundTintList = resources.getColorStateList(R.color.grey2)
+                            setTextColor(resources.getColor(R.color.text_color3))
+                        }
+                    }
+                    "MALE" -> {
+                        buttonGenderMale.run {
+                            backgroundTintList = resources.getColorStateList(R.color.grey2)
+                            setTextColor(resources.getColor(R.color.text_color3))
+                        }
+                    }
+                }
+                buttonGenderOthers.run {
+                    backgroundTintList = resources.getColorStateList(R.color.main)
+                    setTextColor(resources.getColor(R.color.white))
+                }
+                gender = "OTHER"
+            }
+
+            buttonSkip.setOnClickListener {
+                onboardingActivity.supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainerView_onboarding, SignUpPhysicalFragment())
+                    .addToBackStack(null)
+                    .commit()
+            }
+
+            buttonNext.setOnClickListener {
+                MyApplication.signUpInfo?.userDto?.gender = gender.toString()
+                onboardingActivity.supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainerView_onboarding, SignUpPhysicalFragment())
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SignUpGenderFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SignUpGenderFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    fun initView() {
+        binding.run {
+            buttonNext.visibility = View.INVISIBLE
+
+            toolbar.run {
+                buttonBack.setOnClickListener {
+                    fragmentManager?.popBackStack()
                 }
             }
+        }
     }
 }
