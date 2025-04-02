32236997b3ba7bf5a9832bacd110d63651a81598
PIYUJIN <yjyj020201@gmail.com>
2025-04-02 19:04:12 +0900

    [Feat] #23  마이페이지-프로필 수정 화면 구성

--- app/src/main/java/com/project/score/Utils/TimeUtil.kt
+++ app/src/main/java/com/project/score/Utils/TimeUtil.kt
@@ -1,5 +1,10 @@
 package com.project.score.Utils
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+
 object TimeUtil {
     fun formatExerciseTime(seconds: Int): String {
         val hours = seconds / 3600
@@ -19,4 +24,19 @@ object TimeUtil {
             else -> ""
         }
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun formatTimeToKoreanDisplay(time: String): String {
+        val formatter = DateTimeFormatter.ofPattern("HH:mm:ss")
+        val localTime = LocalTime.parse(time, formatter)
+
+        val hour = localTime.hour
+        val minute = localTime.minute
+
+        val amPm = if (hour < 12) "오전" else "오후"
+        val hour12 = if (hour == 0 || hour == 12) 12 else hour % 12
+
+        return "매일 $amPm ${hour12}:${minute.toString().padStart(2, '0')}"
+    }
+
 }
