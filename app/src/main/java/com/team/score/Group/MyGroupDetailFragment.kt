f2640f8d8b849d019ec0dad44ecd42e3c3accb63
PIYUJIN <yjyj020201@gmail.com>
2025-05-04 14:36:46 +0900

    [Feat] #33  내 그룹 상세 화면 랭킹 기능 구현

--- app/src/main/java/com/team/score/Group/MyGroupDetailFragment.kt
+++ app/src/main/java/com/team/score/Group/MyGroupDetailFragment.kt
@@ -9,12 +9,17 @@ import android.view.ViewGroup
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.bumptech.glide.Glide
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import com.team.score.API.response.group.GroupDetailResponse
+import com.team.score.API.response.group.GroupRankingResponse
+import com.team.score.API.response.group.RankerInfo
 import com.team.score.Group.viewModel.GroupViewModel
 import com.team.score.MainActivity
+import com.team.score.R
 import com.team.score.Utils.TimeUtil.formatExerciseTime
+import com.team.score.Utils.TimeUtil.formatSecondsToMinuteString
 import com.team.score.databinding.FragmentMyGroupDetailBinding
 
 class MyGroupDetailFragment : Fragment() {
@@ -22,12 +27,13 @@ class MyGroupDetailFragment : Fragment() {
     lateinit var binding: FragmentMyGroupDetailBinding
     lateinit var mainActivity: MainActivity
     private val viewModel: GroupViewModel by lazy {
-        ViewModelProvider(requireActivity())[GroupViewModel::class.java]
+        ViewModelProvider(this)[GroupViewModel::class.java]
     }
 
     private lateinit var fragmentList: List<Fragment>
     private var groupId: Int = 0
     private var getGroupDetail: GroupDetailResponse? = null
+    private var getGroupRanking: GroupRankingResponse? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -73,6 +79,16 @@ class MyGroupDetailFragment : Fragment() {
                     textViewTodayMemberValue.text = "${getGroupDetail?.numOfExercisedToday}/${getGroupDetail?.numOfTotalMembers} 명"
                 }
             }
+
+            groupRanking.observe(viewLifecycleOwner) {
+                getGroupRanking = it
+
+                binding.run {
+                    layoutMyGroupRanking.run {
+                        applyRankingUI(getGroupRanking?.rankersInfo ?: emptyList())
+                    }
+                }
+            }
         }
     }
 
@@ -108,6 +124,8 @@ class MyGroupDetailFragment : Fragment() {
 
                 }
             }
+
+            applyRankingUI(getGroupRanking?.rankersInfo ?: emptyList())
         }
     }
 
@@ -122,4 +140,87 @@ class MyGroupDetailFragment : Fragment() {
         override fun createFragment(position: Int): Fragment = fragments[position]
     }
 
+    fun applyRankingUI(rankersInfo: List<RankerInfo>) {
+        binding.layoutMyGroupRanking.run {
+            val views = listOf(
+                layoutRanking1,
+                layoutRanking2,
+                layoutRanking3,
+                layoutRanking4,
+                layoutRanking5
+            )
+
+            val graphs = listOf(
+                graphRanking1,
+                graphRanking2,
+                graphRanking3,
+                graphRanking4,
+                graphRanking5
+            )
+
+            val times = listOf(
+                textViewRanking1ExerciseTime,
+                textViewRanking2ExerciseTime,
+                textViewRanking3ExerciseTime,
+                textViewRanking4ExerciseTime,
+                textViewRanking5ExerciseTime
+            )
+
+            val days = listOf(
+                textViewRanking1Days,
+                textViewRanking2Days,
+                textViewRanking3Days,
+                textViewRanking4Days,
+                textViewRanking5Days
+            )
+
+            val profiles = listOf(
+                imageViewRanking1Profile,
+                imageViewRanking2Profile,
+                imageViewRanking3Profile,
+                imageViewRanking4Profile,
+                imageViewRanking5Profile
+            )
+
+            val nicknames = listOf(
+                textViewRanking1Nickname,
+                textViewRanking2Nickname,
+                textViewRanking3Nickname,
+                textViewRanking4Nickname,
+                textViewRanking5Nickname
+            )
+
+            if (rankersInfo.isEmpty()) {
+                graphs.forEach { it.visibility = View.GONE }
+                return@run
+            }
+
+            rankersInfo.forEachIndexed { index, ranker ->
+                if (index >= 5) return@forEachIndexed
+
+                val timeText = formatSecondsToMinuteString(ranker.weeklyExerciseTime)
+                val hasExercise = ranker.weeklyExerciseTime > 0
+
+                times[index].text = if (hasExercise) timeText else ""  // 기록 없으면 시간 텍스트 없음
+                times[index].visibility = if (hasExercise) View.VISIBLE else View.INVISIBLE
+
+                days[index].text = if (index == 0 && hasExercise)
+                    "연속 ${ranker.changedAmount}일 째"
+                else if (hasExercise)
+                    "${ranker.changedAmount}일"
+                else
+                    ""  // 기록 없으면 일수 텍스트도 생략
+
+                Glide.with(profiles[index].context)
+                    .load(ranker.profileImgUrl)
+                    .placeholder(R.drawable.img_profile_null)
+                    .into(profiles[index])
+
+                // 그래프 뷰 자체도 기록 없으면 숨김
+                graphs[index].visibility = if (hasExercise) View.VISIBLE else View.INVISIBLE
+
+                nicknames[index].text = ranker.nickname
+            }
+        }
+    }
 }
