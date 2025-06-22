ec79aa1d25c8b2a1d496f4f2272672b38e737b57
PIYUJIN <yjyj020201@gmail.com>
2025-06-22 18:35:51 +0900

    [Feat] #56  온보딩 화면 GA 추가

--- app/src/main/java/com/team/score/OnBoarding/OnboardingFragment.kt
+++ app/src/main/java/com/team/score/OnBoarding/OnboardingFragment.kt
@@ -9,9 +9,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
+import com.google.firebase.Firebase
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.analytics
 import com.team.score.API.TokenManager
 import com.team.score.Login.LoginFragment
 import com.team.score.R
+import com.team.score.Utils.GlobalApplication
+import com.team.score.Utils.GlobalApplication.Companion.firebaseAnalytics
 import com.team.score.databinding.FragmentOnboardingBinding
 import kotlin.math.min
 
@@ -37,15 +42,14 @@ class OnboardingFragment : Fragment() {
 
                 dotsIndicator.setViewPager2(binding.pager2)
                 buttonNext.setOnClickListener {
+                    firebaseAnalytics.logEvent("click_next_splash", null)
+
                     currentItem = binding.pager2.currentItem
-                    Log.d("##", "current item : ${currentItem}")
 
                     if (currentItem < totalPager - 1) {
-                        Log.d("##", "current item < totalPager")
                         // 현재 페이지가 마지막 페이지가 아니면 다음 페이지로 이동
                         pager2.setCurrentItem(currentItem + 1, true)
                     } else {
-                        Log.d("##", "current item == totalPager")
                         // 마지막 페이지면 버튼을 비활성화하거나 다른 작업 수행
                         TokenManager(onboardingActivity).saveEnter(false)
 
@@ -55,19 +59,14 @@ class OnboardingFragment : Fragment() {
                     }
                 }
                 buttonSkip.setOnClickListener {
+                    firebaseAnalytics.logEvent("click_skip_splash", null)
+
                     TokenManager(onboardingActivity).saveEnter(false)
 
                     onboardingActivity.supportFragmentManager.beginTransaction()
                         .replace(R.id.fragmentContainerView_onboarding, LoginFragment())
                         .commit()
                 }
-
-                // LAYOUT_DIRECTION_RTL : 스크롤 방향 - 오른쪽
-                // LAYOUT_DIRECTION_LTR : 스크롤 방향 - 왼쪽 (기본)
-//                layoutDirection = ViewPager2.LAYOUT_DIRECTION_RTL
-
-                // ORIENTATION_VERTICAL : 스크롤 방향 - 상하
-//                orientation = ViewPager2.ORIENTATION_VERTICAL
 
                 // 페이지가 변경될 때 동작하는 method
                 registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
