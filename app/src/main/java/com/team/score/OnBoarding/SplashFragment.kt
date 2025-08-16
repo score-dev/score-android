01627bdfd1b88177cb4451c044ed8c77dcdf5bb3
PIYUJIN <yjyj020201@gmail.com>
2025-08-16 17:23:26 +0900

    [Feat] #59  자동 로그인 기능 구현

--- app/src/main/java/com/team/score/OnBoarding/SplashFragment.kt
+++ app/src/main/java/com/team/score/OnBoarding/SplashFragment.kt
@@ -1,5 +1,6 @@
 package com.team.score.OnBoarding
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.fragment.app.Fragment
@@ -7,7 +8,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.team.score.API.TokenManager
+import com.team.score.API.TokenUtil
 import com.team.score.Login.LoginFragment
+import com.team.score.MainActivity
 import com.team.score.R
 import com.team.score.databinding.FragmentSplashBinding
 
@@ -25,15 +28,30 @@ class SplashFragment : Fragment() {
         onboardingActivity = activity as OnboardingActivity
 
         Handler().postDelayed({
-            val nextFragment = if(TokenManager(onboardingActivity).getEnter()) {
-                OnboardingFragment()
+            if(TokenManager(onboardingActivity).getEnter()) {
+                onboardingActivity.supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainerView_onboarding, OnboardingFragment())
+                    .commit()
             } else {
-                LoginFragment()
+                if(TokenManager(onboardingActivity).getRefreshToken().isNullOrEmpty()) {
+                    onboardingActivity.supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView_onboarding, LoginFragment())
+                        .commit()
+                } else {
+                    TokenUtil.refreshToken(
+                        onboardingActivity,
+                        retryRequest = {
+                            val mainIntent = Intent(activity, MainActivity::class.java)
+                            mainIntent.putExtra("isLogin", true)
+                            onboardingActivity.startActivity(mainIntent) },
+                        onFailure = {
+                            onboardingActivity.supportFragmentManager.beginTransaction()
+                                .replace(R.id.fragmentContainerView_onboarding, LoginFragment())
+                                .commit()
+                        }
+                    )
+                }
             }
-
-            val transaction = onboardingActivity.supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.fragmentContainerView_onboarding, nextFragment)
-            transaction.commit()
         }, 3000)
 
         return binding.root
