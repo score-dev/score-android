7828b982aaec2e75e659b8c880c4fbc55ae35628
PIYUJIN <yjyj020201@gmail.com>
2025-08-16 17:16:36 +0900

    [Feat] #59  온보딩 - 초대 화면 카카오톡 초대 기능 구현

--- app/src/main/java/com/team/score/SignUp/SignUpLinkFragment.kt
+++ app/src/main/java/com/team/score/SignUp/SignUpLinkFragment.kt
@@ -1,10 +1,15 @@
 package com.team.score.SignUp
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.kakao.sdk.share.ShareClient
+import com.kakao.sdk.share.WebSharerClient
 import com.team.score.API.weather.response.Main
 import com.team.score.MainActivity
 import com.team.score.OnBoarding.OnboardingActivity
@@ -29,6 +34,7 @@ class SignUpLinkFragment : Fragment() {
         binding.run {
             buttonKakao.setOnClickListener {
                 // 카카오 초대
+                shareToKakaoWithCustomTemplate(requireContext())
             }
 
             buttonLink.setOnClickListener {
@@ -73,6 +79,26 @@ class SignUpLinkFragment : Fragment() {
                     fragmentManager?.popBackStack()
                 }
             }
+        }
+    }
+
+    fun shareToKakaoWithCustomTemplate(context: Context) {
+        val templateId = 123384L
+
+        if (ShareClient.instance.isKakaoTalkSharingAvailable(context)) {
+            ShareClient.instance.shareCustom(context, templateId, null) { result, error ->
+                if (error != null) {
+                    Log.e("KakaoShare", "카카오톡 커스텀 템플릿 공유 실패", error)
+                } else if (result != null) {
+                    Log.d("KakaoShare", "공유 성공: ${result.intent.data}") // ✅ 딥링크 URI 로그
+                    context.startActivity(result.intent)
+                }
+            }
+        } else {
+            WebSharerClient.instance.makeCustomUrl(templateId, null)?.let { url ->
+                Log.d("KakaoShare", "웹 공유 URI: $url")
+                context.startActivity(Intent(Intent.ACTION_VIEW, url))
+            } ?: Log.e("KakaoShare", "웹 공유 URL 생성 실패")
         }
     }
 
